5803be30fc9969e445fd29cb7a7b28d26478c2e9
Pito Desri Pauzi <povat0171@gmail.com>
2025-01-17 11:43:36 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,38 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Pencatatan Pengeluaran",
-  description: "Web untuk mencatat pengeluaran dan pemasukan anda."
+  title: "PecPeng - Pencatatan Pengeluaran",
+  description: "Web untuk mencatat pengeluaran dan pemasukan anda.",
+  icons: "/logo.png",
+  verification: {
+    google: "7UVWLjtHfAA1ccchiarJNOFElAmh6e2a4nHTqbosAZU"
+  },
+  keywords: [
+    'Catat Pemasukkan', 'Catat Pengeluaran', 'PecPeng', 'Backend', 'React', 'Next.js', 'Laravel', 'Node.js', 'Prisma', 'PostgreSQL', 'MySQL', 'JavaScript', 'TypeScript', 'API Development', 'Database Management', 'Version Control', 'GitHub', 'User Authentication', 'JWT'
+  ],
+  openGraph: {
+    title: 'PecPeng - Pencatatan Pengeluaran',
+    description: 'Web untuk mencatat pengeluaran dan pemasukan anda.',
+    url: 'https://catatan-pengeluaran-three.vercel.app/',
+    siteName: 'PecPeng - Pencatatan Pengeluaran',
+    images: ['https://catatan-pengeluaran-three.vercel.app/logo.png'],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      "max-snippet": -1
+    },
+  },
+  authors: [{
+    name: 'Pito Desri Pauzi', url: 'https://github.com/PitokDf'
+  }],
 };
 
 export default function Layout({
